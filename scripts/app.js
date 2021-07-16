588c6304c5f419a1ee6da3b756fdd183d1f5c554
Jessica Strawford <jessicastrawford@gmail.com>
2021-07-16 09:07:52 +0100

    step 1 complete- making the two grids. I have a working start button that stops working once it has been clicked once (before if I clicked twice it moved up the board twice and if I clicked it three times it moved three etc etc) - So I've fixed my first bug! I made my starting position for my artist and I then tried to make his base (game complete squares) but I struggled to make his base- I intend to go back to this this morning. I then went on to think about my moving blocks- even though in the end I visualise these being moving paintbrushes I have started with just colored blocks for now. I have got as far as starting them but I need to figure out how to stop them after three classes have filled with pink and once this has happenned delete the old squares- the movement of them horizontally across the board. I thought about for loops, using setTimeOut, if else/ or switch statements. This is my goal for this morning is to complete the artsits home and create my moving blocks. I did also come across online that keyCode isn't compatable on some browsers now and to avoid it, so I changed this to just key but it broke my code. So it's still in there for now but I need to revisit this and change it. I also need to work out visually how I can have my squares on the full page as when I click up it moves my artist and the window at the same time.

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,145 @@
-console.log('Hello World')
+
+// Dom Elements 
+const gridOne = document.querySelector('.grid-one')
+const gridTwo = document.querySelector('.grid-two')
+const startBtn = document.querySelector('#start')
+
+// Grid Variables 
+const width = 11
+const cellCount = width * width
+const cells = []
+
+// Game Variables
+const artistClass = 'artist'
+let artistPosition = 115
+let isPlaying = false 
+
+// Grid Functions
+
+function createGridOne() {
+  for (let i = 0; i < cellCount; i++) {
+    const cell = document.createElement('div')
+    cell.textContent = i
+    gridOne.appendChild(cell)
+    cells.push(cell)
+  }
+}
+
+createGridOne()
+
+function createGridTwo() {
+  for (let i = 0; i < cellCount; i++) {
+    const cell = document.createElement('div')
+    cell.textContent = i
+    gridTwo.appendChild(cell)
+    cells.push(cell)
+  }
+}
+
+createGridTwo()
+
+// Functions
+
+function addArtist() {
+  cells[artistPosition].classList.add(artistClass)
+}
+
+function removeArtist() {
+  cells[artistPosition].classList.remove(artistClass)
+}
+
+function handleStart(){
+
+  let currentIndex = 87
+  setInterval(() => {
+    cells[currentIndex].classList.add('pink')
+    currentIndex --
+  }, 500)
+
+  // want to say if the number of cells with a class of pink > 3 then delete the class of pink from the start, do you use a for loop?
+  // - Stop adding classes after 1.5 seconds (1500)
+
+
+
+  console.log('start')
+  addArtist()
+  if (isPlaying) return
+  isPlaying = true 
+  function handleKeyUp(event) {
+    removeArtist(artistPosition) 
+
+    const x = artistPosition % width
+    const y = Math.floor(artistPosition / width)
+
+    switch (event.keyCode) {
+      case 39:
+        if (x < width - 1) 
+          artistPosition ++
+        break
+      case 37:
+        if (x > 0) artistPosition --
+        break
+      case 38:
+        if (y > 0) artistPosition -= width
+        break
+      case 40:
+        if (y < width - 1) artistPosition += width
+        break
+      default:
+        console.log('invalid key do nothing')
+    }
+    addArtist(artistPosition)
+
+
+
+  }
+  document.addEventListener('keyup', handleKeyUp)
+
+}
+
+
+
+
+// Events
+
+startBtn.addEventListener('click', handleStart)
+
+
+
+
+
+// function handlePlay() {
+//   if (cells[artistPosition] === 3 || 7){
+//     alert('Sorry you got hit, game over!')
+//     window.location.reload() 
+//   } else {
+//     console.log('Keep playing')
+//   } 
+//   }
+//   handlePlay()
+
+
+// function handleEnd() {
+//   if() {
+//     cells[artistPosition] === 3 || 7 
+//     console.log('You win')
+//   }
+//   }
+
+//   handleEnd()
+
+// function handleEnd(){
+//   if(artistPosition = 0 || 7) {
+//     console.log('You Win!!')
+//   }
+// }
+// handleEnd()
+
+  // if (cells[artistPosition] === 3 || 7){
+     //   alert('Sorry you got hit, game over!')
+      //   window.location.reload() 
+      // } else {
+      //   console.log('Keep playing')
+      // } 
+      
+      // handlePlay()
